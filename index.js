628fa23c09fae0d50427bfbca263a46809b86c17
Nic Travis <nptravis@yahoo.com>
2018-01-29 02:10:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ function createRock(x) {
       window.requestAnimationFrame(moveRock);
      }
      else {
-       GAME.removeChild(ROCKS[k]);
+       GAME.removeChild(ROCKS[ROCKS.length-1]);
      }
 
     /**
